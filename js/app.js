5b432b37fa00d2d66d4e51bf6ad27a350e28e670
Limsa00 <bennis.salim@gmail.com>
2021-08-16 12:29:04 +0200

    handle pixel color and implementing form config

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
 var app = {
+    generateForm:()=>{
+        var form = document.getElementById('form');
+
+        var input = document.createElement('input');
+        input.classList.add('form-input');
+        input.placeholder = 'Définir taille de grille';
+        input.type = 'number';
+        input.value = 5;
+        input.min = 1;
+        input.max = 20;
+
+        var button = document.createElement('button');
+        button.classList.add('form-button');
+        button.textContent ='Valider';
+
+        form.appendChild(input);
+        form.appendChild(button);
+    },
 
     createGridLine:()=>{
         app.gridLine = document.createElement('div');
         app.gridLine.classList.add('gridLine');
 
-        var invader = document.getElementById('invader');
-        invader.appendChild(app.gridLine);
+        app.invader = document.getElementById('invader');
+        app.invader.appendChild(app.gridLine);
     },
 
     createPixel:()=>{
-        var pixel = document.createElement('div');
-        pixel.classList.add('pixel');
+        app.pixel = document.createElement('div');
+        app.pixel.classList.add('pixel');
+        app.pixel.classList.add('grey');
     
-        app.gridLine.appendChild(pixel);
+        app.gridLine.appendChild(app.pixel);
     },
 
     createGrid:(nbLine, nbPixel)=>{            
@@ -20,12 +39,31 @@ var app = {
             app.createGridLine();
 
             for (let index = 0; index < nbPixel; index++) {
-                app.createPixel();                
+                app.createPixel();
+                app.pixel.addEventListener('click', app.changeColorPixel);
             };            
         };
     },
 
+    changeColorPixel:(event)=>{
+        var pixel = event.target;
+
+        if (pixel.classList.contains('grey')){
+            pixel.classList.remove('grey');
+            pixel.classList.add('white');
+        } 
+        else if(pixel.classList.contains('white')){
+            pixel.classList.remove('white');
+            pixel.classList.add('black');
+        }
+        else if (pixel.classList.contains('black')){
+            pixel.classList.remove('black');
+            pixel.classList.add('white');
+        }
+    },
+
     init:()=>{
+        app.generateForm();
         app.createGrid(5,5);
     }
 };
